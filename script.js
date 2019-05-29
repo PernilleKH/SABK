9bf11bbe3f6faf1cf30d5daf5c313216dcd3c517
SarahCClausen <43234263+SarahCClausen@users.noreply.github.com>
2019-05-29 14:44:59 +0200

    Lidt af hvert

---
+++ script.js
@@ -0,0 +1,22 @@
+var myIndex = 0; // start med at nulstille myIndex. myIndex bruges til at tælle og holde styr på hvilket billede vi er nået til.
+var fotos = document.querySelectorAll(".foto"); // Find i DOM træet alle elementer med class "foto"
+
+carousel(); // Kald funktionen og start showet :D
+
+function carousel() {
+  // loop alle fotos igennem
+  for (var i = 0; i < fotos.length; i++) { // for hvert af fotos
+    fotos[i].style.display = "none";  // sæt display til "none", som dermed fjerner billedet fra renderingen
+  }
+  if (myIndex > fotos.length) {myIndex = 0} // hvis vores tæller er størrere end antallet af fotos, sæt tæller til 1   
+  fotos[myIndex].style.display = "unset"; // fjern display:none fra det billede vi vil vise  
+  myIndex++; // læg een til vores tæller
+  setTimeout(carousel, 2500); // Gentag denne funktion hver 2.5 sekund
+}
+
+
+
+
+
+
+
